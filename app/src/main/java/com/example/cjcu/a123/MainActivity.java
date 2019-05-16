8fef90ef2d5d75410be58a88f4e70a2621e448d7
ffffff <j6jp6rmp0325@gmail.com>
2019-05-16 13:10:32 +0800

    加入 Spinner

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.cjcu.a123;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Spinner sp_fruite;
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button button = findViewById(R.id.button);
+        button = findViewById(R.id.button);
 
+        sp_fruite = findViewById(R.id.sp_fruit);
     }
 }
