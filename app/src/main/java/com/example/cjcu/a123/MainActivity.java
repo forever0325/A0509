4f5593a7ea44c15c2cf1f6dda22d43608151be61
ffffff <j6jp6rmp0325@gmail.com>
2019-05-16 13:11:21 +0800

    加入 Spinner

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -15,7 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-
+ 
         sp_fruite = findViewById(R.id.sp_fruit);
     }
 }
