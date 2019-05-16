49a5bf2a0ffb53586ed365c084227b63870c44b8
ffffff <j6jp6rmp0325@gmail.com>
2019-05-16 13:44:36 +0800

    加入 Spinner的資料

--- app/src/main/java/com/example/cjcu/a123/MainActivity.java
+++ app/src/main/java/com/example/cjcu/a123/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.cjcu.a123;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 
@@ -10,12 +11,15 @@ public class MainActivity extends AppCompatActivity {
     private Spinner sp_fruite;
     private Button button;
 
+    String[] fruite= {"apple","banana","奇異果"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button = findViewById(R.id.button);
-  
         sp_fruite = findViewById(R.id.sp_fruit);
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,fruite);
+        sp_fruite.setAdapter(adapter);
     }
 }
